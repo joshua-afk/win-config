30d2736ad741c294f1e0cfc234f791020ad2fa09
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-06-13 12:50:50 +0800

    feat(nvim): install git-blame

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,11 +16,17 @@ require 'mappings'
 -------- AUTO-COMMANDS --------
 require 'auto-commands'
 
+-------- NEOVIDE SETTINGS --------
 if vim.g.neovide then
   opt.guifont = { "CaskaydiaCove NF", ":h10" }
-  vim.opt.linespace=5
+  -- vim.opt.linespace=3
 
   -- vim.g.neovide_cursor_vfx_mode = "ripple"
   -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
   vim.g.neovide_cursor_vfx_mode = "torpedo"
 end
+
+-------- VIM PLUG PLUGIN SETTINGS --------
+vim.cmd [[
+  let g:gitblame_enabled = 0
+]]
