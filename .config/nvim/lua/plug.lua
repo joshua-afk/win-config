d1767ff29868bae2b165ed9c1d78a0e9aeff3b85
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-06-13 12:54:02 +0800

    refactor(nvim): plug git-blame

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -7,7 +7,7 @@ vim.call('plug#begin', fn.stdpath('data')..'/plugged')
 Plug 'chrisbra/vim-diff-enhanced'
 Plug 'junegunn/vim-easy-align'
 Plug 'NMAC427/guess-indent.nvim'
-Plug 'lukas-reineke/indent-blankline.nvim'
+-- Plug 'lukas-reineke/indent-blankline.nvim'
 
 -- Navigation
 Plug('mg979/vim-visual-multi', {branch = 'master'})
@@ -39,7 +39,7 @@ Plug 'ap/vim-css-color'
 
 -- Git
 Plug 'tpope/vim-fugitive'
---Plug 'f-person/git-blame.nvim'
+Plug 'f-person/git-blame.nvim'
 
 -- Theme
 Plug 'rakr/vim-one'
