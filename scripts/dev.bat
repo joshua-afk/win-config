bf6105af26f230ca38d16ae866f5ae25ee134af9
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-07-09 16:03:06 +0800

    feat(scripts): update dev script for new workflow

--- scripts/dev.bat
+++ scripts/dev.bat
@@ -1,4 +1,8 @@
 @echo off
 start \"Program Files\Firefox Developer Edition\firefox.exe
+start \"Program Files\WindowsApps\Microsoft.WindowsTerminalPreview_1.18.1462.0_x64__8wekyb3d8bbwe\wt.exe
 start cmder.exe
-start wezterm-gui.exe
+
+start \Users\jr.velasquez.JEONSOFT\AppData\Local\SourceTree\SourceTree.exe
+start \"Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe
+start \"Users\jr.velasquez.JEONSOFT\AppData\Local\Obsidian\Obsidian.exe
