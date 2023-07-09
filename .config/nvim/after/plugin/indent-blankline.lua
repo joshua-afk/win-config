a0643625a352baacd1dc6856d36a777ea73b6915
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-07-09 15:53:13 +0800

    feat(nvim): disable indent-blankline plugin

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,12 +1,12 @@
-opt = vim.opt
-
-opt.list = true
-opt.listchars:append "space:⋅"
-opt.listchars:append "eol:↴"
-
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
-    space_char_blankline = " ",
-}
+-- opt = vim.opt
+-- -- opt.list = true
+-- -- opt.listchars:append "space:⋅"
+-- -- opt.listchars:append "eol:↴"
+--
+-- require("indent_blankline").setup {
+--   indent_blankline_enabled = false,
+--   -- for example, context is off by default, use this to turn it on
+--   -- show_current_context = true,
+--   -- show_current_context_start = true,
+--   -- space_char_blankline = " ",
+-- }
