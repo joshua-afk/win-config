8c496f16d0c309c4ffae63e49a519719d47f0418
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-08-01 12:41:00 +0800

    feat(nvim): reconfig indent blankline

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,12 +1,32 @@
--- opt = vim.opt
--- -- opt.list = true
--- -- opt.listchars:append "space:⋅"
--- -- opt.listchars:append "eol:↴"
---
--- require("indent_blankline").setup {
---   indent_blankline_enabled = false,
---   -- for example, context is off by default, use this to turn it on
---   -- show_current_context = true,
---   -- show_current_context_start = true,
---   -- space_char_blankline = " ",
--- }
+opt = vim.opt
+opt.list = true
+
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#f5c2e7 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#f38ba8 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#fab387 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#a6e3a1 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#89dceb gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#89b4fa gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent7 guifg=#cdd6f4 gui=nocombine]]
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "eol:↴"
+
+-- opt.listchars:append "space:⋅"
+-- opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+  indent_blankline_enabled = false,
+  -- char_highlight_list = {
+  --   "IndentBlanklineIndent1",
+  --   "IndentBlanklineIndent2",
+  --   "IndentBlanklineIndent3",
+  --   "IndentBlanklineIndent4",
+  --   "IndentBlanklineIndent5",
+  --   "IndentBlanklineIndent6",
+  --   "IndentBlanklineIndent7",
+  -- },
+  -- for example, context is off by default, use this to turn it on
+  -- show_current_context = true,
+  -- show_current_context_start = true,
+  space_char_blankline = " ",
+}
