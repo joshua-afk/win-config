99e45e3680c171c0f76fe19d9f36be9e4ddcb297
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-11-07 17:48:04 +0800

    revert(nvim): use snipmate snippets instead of lua

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -17,37 +17,39 @@ ls.setup({
   },
 })
 
--- Keymaps
-vim.cmd [[
-  " press <Tab> to expand or jump in a snippet. These can also be mapped separately
-  " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-  imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-  " -1 for jumping backwards.
-  inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-  snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-  snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
-
-  " For changing choices in choiceNodes (not strictly necessary for a basic setup).
-  imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-  smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-]]
-
--- extend snippets
-ls.filetype_extend("javascript", { "javascript-custom", "javascript-moment", "javascript-node", "javascript-mssql"})
-
-require("luasnip.loaders.from_lua").load({paths = lua_snip_path})
+-- -- Keymaps
+-- vim.cmd [[
+--   " press <Tab> to expand or jump in a snippet. These can also be mapped separately
+--   " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+--   imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+--   " -1 for jumping backwards.
+--   inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+--
+--   snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+--   snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+--
+--   " For changing choices in choiceNodes (not strictly necessary for a basic setup).
+--   imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+--   smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+-- ]]
+--
+-- -- extend snippets
+-- ls.filetype_extend("javascript", { "javascript-custom", "javascript-moment", "javascript-node", "javascript-mssql"})
+--
+-- require("luasnip.loaders.from_lua").load({paths = lua_snip_path})
 
 -- SNIPMATE FORMAT
 -- extend snippets
--- ls.filetype_extend("lua",        {"references"})
--- ls.filetype_extend("ruby",       {"references"})
--- ls.filetype_extend("blade",      {"references"})
--- ls.filetype_extend("javascript", {"references", "javascript-moment", "javascript-node", "javascript-webix", "javascript-express", "javascript-axios"})
--- ls.filetype_extend("blade",      {"references", "html"})
--- ls.filetype_extend("html",       {"references"})
--- ls.filetype_extend("eruby",      {"references", "html"})
--- ls.filetype_extend("sql",        {"references", "sql-jeonsoft"})
--- ls.filetype_extend("css",        {"references"})
---
--- require("luasnip.loaders.from_snipmate").load({ paths = { snipmate_snip_path } })
+ls.filetype_extend("lua",        {"references"})
+ls.filetype_extend("ruby",       {"references"})
+ls.filetype_extend("blade",      {"references"})
+ls.filetype_extend("javascript", {"references", "javascript-moment", "javascript-node", "javascript-webix", "javascript-express", "javascript-axios"})
+ls.filetype_extend("blade",      {"references", "html"})
+ls.filetype_extend("html",       {"references"})
+ls.filetype_extend("eruby",      {"references", "html"})
+ls.filetype_extend("sql",        {"references", "sql-jeonsoft"})
+ls.filetype_extend("css",        {"references"})
+ls.filetype_extend("scss",        {"references", "css"})
+ls.filetype_extend("sass",        {"references", "css"})
+
+require("luasnip.loaders.from_snipmate").load({ paths = { snipmate_snip_path } })
