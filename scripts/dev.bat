4c0a9c23ae09fde2002c1a1ee81801893f1d27cd
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2023-12-04 09:38:05 +0800

    feat(scripts): add ads and refactor script

--- scripts/dev.bat
+++ scripts/dev.bat
@@ -1,24 +1,28 @@
 @echo off
 
 set app_data="%USERPROFILE%\AppData\Local"
+
 :: Firefox
 start \"Program Files\Firefox Developer Edition\firefox.exe
 
 :: Windows Terminal
-start \"Program Files\WindowsApps\Microsoft.WindowsTerminalPreview_1.19.2682.0_x64__8wekyb3d8bbwe\wt.exe
+:: start \"Program Files\WindowsApps\Microsoft.WindowsTerminalPreview_1.19.2682.0_x64__8wekyb3d8bbwe\wt.exe
+%app_data%\Microsoft\WindowsApps\wt.exe
 
 :: Cmder
-::start cmder.exe
+start cmder.exe
 
-:: SourceTreej
+:: SourceTree
 ::start %app_data%\SourceTree\SourceTree.exe
 
 :: Git Fork
 %app_data%\Fork\fork.exe
 
 :: MS SQL
-start \"Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe
+:: start \"Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe
+
+:: ADS
+%app_data%\"Programs\Azure Data Studio\azuredatastudio.exe
 
 :: Obsidian
 %app_data%\Obsidian\Obsidian.exe
-
