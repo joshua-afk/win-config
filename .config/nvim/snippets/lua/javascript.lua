26eff9b754160b6cc8ce3fa4d2b35c08f71cb682
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2024-01-15 14:26:19 +0800

    chore(nvim): snippets mass update

--- .config/nvim/snippets/lua/javascript.lua
+++ .config/nvim/snippets/lua/javascript.lua
@@ -1,3 +1,13 @@
+-------------------------------
+-- Contents
+--
+-- Common
+-- Conditions
+-- Loops
+-- Objects
+--
+-------------------------------
+
 local ls = require 'luasnip'
 
 local s = ls.snippet
@@ -5,12 +15,63 @@ local t = ls.text_node
 local i = ls.insert_node
 local rep = require("luasnip.extras").rep
 local fmt = require("luasnip.extras.fmt").fmt
+local ps = ls.parser.parse_snippet
 
 return {
-  ls.parser.parse_snippet(
-    "cl",
+  -- Conditions
+  ps(
+    "if",
+		"if (${1}) {\n\t${0}\n}"
+	),
+
+  ps(
+    "ifelse",
+		"if (${1}) {\n\t${2}\n} else {\n\t${0}\n}"
+	),
+
+  ps(
+    "switch",
+		"switch(${1}) {\n\tcase x:\n\t\t// ...\n\t\tbreak;\n\tcase y:\n\t\t// ..\n\t\tbreak;\n\tdefault:\n}"
+	),
+  --array true
+
+  -- Loops
+  -- for loop
+  ps(
+    "forin",
+		"for (const i in ${1}) {\n\t${0}\n}"
+	),
+
+  ps(
+    "forof",
+		"for (const key of ${1}) {\n\t${0}\n}"
+	),
+  --foreach
+
+  -- Common
+  -- s(
+  --   "cl",
+  --   fmt("console.log('{}', {})", { i(1, ''), rep(1) })
+  -- ),
+  ps(
+    "clv",
 		"console.log(${1:'Hi mom!'})"
 	),
 
-  s('clv', fmt("console.log('{}', {})", { i(1, ''), rep(1) })),
+  -- s("fn", {
+  --   c(1, {
+		-- 	t("const tmpFunction = _ => "),
+		-- 	t("const tmpFunction = () => "),
+		-- 	t("function tmpFunction() "),
+  --   }),
+  --   t("{"),
+  --   i(2),
+  --   t("}")
+  -- }),
+
+  -- Objects
+  ps(
+    "delete",
+		"delete Employee.firstname;"
+	),
 }
